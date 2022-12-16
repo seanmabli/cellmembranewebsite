77836fef072f281e92de90d28e3482c23813a1c8
Adamimoka <adamimoka@gmail.com>
2022-12-16 19:23:32 +0000

    im dum

---
+++ src/components/homebar.js
@@ -0,0 +1,5 @@
+export function Homebar() {
+    return <div>
+            <h1>Science Website</h1>
+    </div>;
+}
--- src/components/topbar.js
+++
@@ -1,9 +0,0 @@
-export function Topbar() {
-    return <div>
-            <h1>Welcome to my website</h1>
-            <p>Coded by Adam Mokdad and Sean Mabli</p>
-            <p>This interactive site will contain information about cell membrane structure.</p>
-            <p>Make use of the interactive features to learn more about the cell membrane.</p>
-            <p>Created 2022 for Mrs. Shopiro's Advanced Biology class.</p>
-    </div>;
-}
