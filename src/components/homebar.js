bd715109161b9c5efffe34e861f093e50d7a03b3
SeanMabli <smdrone1@gmail.com>
2022-12-19 18:27:11 -0500

    add styling package (mui)

--- src/components/homebar.js
+++
@@ -1,5 +0,0 @@
-export function Homebar() {
-    return <div>
-            <h1>Science Website</h1>
-    </div>;
-}
--- src/home.js
+++ src/home.js
@@ -1,9 +1,20 @@
 export function Home() {
-    return <div>
-            <h1>Welcome to the interactive cell membrane site.</h1>
-            <p>Coded by: Sean Mabli, Adam Mokdad, Arjun Goray, Jagan Mranal, Zain Khan</p>
-            <p>This interactive site will contain information about cell membrane structure.</p>
-            <p>Make use of the interactive features to learn more about the cell membrane.</p><br><br></br></br>
-            <p>Created 2022 for Mrs. Shopiro's Advanced Biology class.</p>
-    </div>;
+  return (
+    <div>
+      <h1>Welcome to the interactive cell membrane site.</h1>
+      <p>
+        Coded by: Sean Mabli, Adam Mokdad, Arjun Goray, Jagan Mranal, Zain Khan
+      </p>
+      <p>
+        This interactive site will contain information about cell membrane
+        structure.
+      </p>
+      <p>
+        Make use of the interactive features to learn more about the cell
+        membrane.
+      </p>
+      <br />
+      <p>Created 2022 for Mrs. Shopiro's Advanced Biology class.</p>
+    </div>
+  );
 }
