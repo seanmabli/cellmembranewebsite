6729570c8c564592484eb8a5061e04df0110daf7
sean mabli <70709738+seanmabli@users.noreply.github.com>
2023-01-07 00:36:03 +0000

    starting to work on pages, finish navigation
    Co-authored-by: arjungoray <arjungoray@users.noreply.github.com>
    Co-authored-by: Jaganmranal <Jaganmranal@users.noreply.github.com>

--- src/introduction/locationofmembrane.js
+++ src/introduction/locationofmembrane.js
@@ -1,3 +1,23 @@
+import "./locationofmembrane.css";
+
 export function IntroductionLocationOfMembrane() {
-    return <p>placeholder</p>;
+  return (
+    <>
+      <h>Where are Membranes Found?</h>
+      <p>
+        While all cells are surrounded by a
+        <strong>
+          <u>plasma membrane</u>
+        </strong>
+        , eukaryotic cells possess additional internal membranes that surround
+        organelles like the nucleus, the endoplasmic reticulum, the Golgi
+        apparatus, mitochondria, and lysosomes. The plasma membrane keeps a cell
+        intact by holding all of the cellular components inside. It also keeps
+        many unwanted molecules outside of the cell. Internal membranes are
+        crucial for creating micro-environments containing sequestered enzymes
+        or high concentrations of hydrogen ions (low pH). Five general
+        categories of membrane function are illustrated in the next section.{" "}
+      </p>
+    </>
+  );
 }
