726511282a1af8274540f73dd8feb6b7699c02ea
sean mabli <smdrone1@gmail.com>
2023-01-12 12:51:27 +0000

    work on css

--- website/src/membraneproteins/integralandperipheral.js
+++ website/src/membraneproteins/integralandperipheral.js
@@ -14,55 +14,34 @@ export function MembraneProteinIntegralAndPeripheral() {
         <div className="paragraphs3">
           <h1>Membrane Proteins are Integral or Peripheral</h1>
           <p>
-            &nbsp; &nbsp; &nbsp;Membrane proteins are crucial both for membrane
-            <br />
-            function and membrane stability. Different membranes
-            <br />
-            differ radically in their protein content. For example,
-            <br />
-            the protein content of the{" "}
+            Membrane proteins are crucial both for membrane function and
+            membrane stability. Different membranes differ radically in their
+            protein content. For example, the protein content of the{" "}
             <span style={{ textDecoration: "underline" }}>
               <strong>plasma membrane</strong>
             </span>{" "}
-            is typically
-            <br />
-            50%, while the protein content of{" "}
+            is typically 50%, while the protein content of{" "}
             <span style={{ textDecoration: "underline" }}>
               <strong>organelle</strong>
             </span>{" "}
-            membranes
-            <br />
-            involved in energy generation (i.e. the internal
-            <br />
-            membranes of mitochondria and chloroplasts) is
-            <br />
-            upwards of 75%.
-            <br />
-            &nbsp; &nbsp; &nbsp;Proteins that are bound to only one side of the
-            lipid
-            <br />
+            membranes involved in energy generation (i.e. the internal membranes
+            of mitochondria and chloroplasts) is upwards of 75%.
+            <p> </p>Proteins that are bound to only one side of the lipid
             bilayer are called{" "}
             <span style={{ textDecoration: "underline" }}>
               <strong>peripheral membrane proteins</strong>
             </span>
-            .<br />
-            Proteins that interact directly with the hydrophobic
-            <br />
-            portion of membranes are termed{" "}
+            . Proteins that interact directly with the hydrophobic portion of
+            membranes are termed{" "}
             <span style={{ textDecoration: "underline" }}>
               <strong>integral membrane</strong>
             </span>
-            <br />
             <span style={{ textDecoration: "underline" }}>
               <strong>proteins</strong>
             </span>
-            . In some cases, these integral membrane
-            <br />
-            proteins pass completely through a membrane. In other
-            <br />
-            cases, these proteins interact with the membrane via a<br />
-            portion embedded in only one monolayer of the
-            <br />
+            . In some cases, these integral membrane proteins pass completely
+            through a membrane. In other cases, these proteins interact with the
+            membrane via a portion embedded in only one monolayer of the
             membrane.
           </p>
         </div>
@@ -81,32 +60,21 @@ export function MembraneProteinIntegralAndPeripheral() {
         <div className="paragraphs3">
           <h1>Transmembrane Domains are Helices</h1>
           <p>
-            &nbsp; &nbsp; &nbsp;Most integral membrane proteins span the lipid
-            bilayer
-            <br />
-            with either a single{" "}
+            Most integral membrane proteins span the lipid bilayer with either a
+            single{" "}
             <span style={{ textDecoration: "underline" }}>
               <strong>helix</strong>
             </span>{" "}
-            (often called a single-pass
-            <br />
-            transmembrane proteins) or several helices (multipass
-            <br />
-            transmembrane proteins). The membrane-spanning
-            <br />
+            (often called a single-pass transmembrane proteins) or several
+            helices (multipass transmembrane proteins). The membrane-spanning
             helices, called{" "}
             <span style={{ textDecoration: "underline" }}>
               <strong>transmembrane domains</strong>
             </span>
-            , interact with
-            <br />
-            the hydrophobic tails of membrane lipids and anchor
-            <br />
-            the protein in the membrane. The parts of the protein
-            <br />
-            that are exposed to water on either side of the
-            <br />
-            membrane are largely hydrophilic.
+            , interact with the hydrophobic tails of membrane lipids and anchor
+            the protein in the membrane. The parts of the protein that are
+            exposed to water on either side of the membrane are largely
+            hydrophilic.
           </p>
         </div>
         <img className="image3" src={integralperipheral} />
@@ -124,33 +92,22 @@ export function MembraneProteinIntegralAndPeripheral() {
         <div className="paragraphs3">
           <h1>Proteins are Asymmetric</h1>
           <p>
-            &nbsp; &nbsp; &nbsp;In most cases, proteins are asymmetric, and
-            their
-            <br />
-            orientation is critical for their function. For example, a<br />
-            protein that interacts with extracellular substances may
-            <br />
-            have a large region exposed to the outside of the cell but
-            <br />
-            only a small polypeptide portion exposed to the{" "}
+            In most cases, proteins are asymmetric, and their orientation is
+            critical for their function. For example, a protein that interacts
+            with extracellular substances may have a large region exposed to the
+            outside of the cell but only a small polypeptide portion exposed to
+            the{" "}
             <span style={{ textDecoration: "underline" }}>
               <strong>cytosol</strong>
             </span>
-            .<br />
-            The particular orientation of a protein is established
-            <br />
-            during its synthesis. Due to the extensive polar regions
-            <br />
-            on either side of the membrane, integral membrane
-            <br />
-            proteins do not flip transversely through the
-            <br />
-            hydrophobic interior of the membrane. This barrier to
-            <br />
+            . The particular orientation of a protein is established during its
+            synthesis. Due to the extensive polar regions on either side of the
+            membrane, integral membrane proteins do not flip transversely
+            through the hydrophobic interior of the membrane. This barrier to
             flipping preserves the asymmetry of the membrane.
           </p>
         </div>
-        <img className="image4" src={integralperipheral} />
+        <img className="imag3" src={integralperipheral} />
         <Pagination
           count={3}
           shape="rounded"
