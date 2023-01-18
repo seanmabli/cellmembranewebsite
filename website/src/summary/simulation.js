298703280a578dff3be31cb0c83027e0cd36a53c
sean mabli <smdrone1@gmail.com>
2023-01-18 02:54:24 +0000

    instructions for simulation

--- website/src/summary/simulation.js
+++ website/src/summary/simulation.js
@@ -1,9 +1,18 @@
 export function Simulation() {
-    return (
-      <div>
-        <h1>Simulation</h1>
-        <iframe src="https://embedcellmembranetutorial.web.app/" width="900px" height="600px" style={{border: "0px"}}></iframe>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h1>Simulation</h1>
+      <p>
+        Controls: press "a" for new water, press "b" for new phospholipid, press
+        "c" for new oil, press "x" over a molecule to delete, press "shift + x"
+        to clear
+      </p>
+      <iframe
+        src="https://embedcellmembranetutorial.web.app/"
+        width="900px"
+        height="600px"
+        style={{ border: "0px" }}
+      ></iframe>
+    </div>
+  );
+}
