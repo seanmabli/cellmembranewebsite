378d9ffd81be55b686d54f1667cc83de92cd2541
laptop <laptop@Desktop-2Z2SQV2.hbcommunications.us>
2023-01-17 21:48:51 -0500

    definitions

--- website/src/membranelipids/membranefluidity1.js
+++ website/src/membranelipids/membranefluidity1.js
@@ -16,7 +16,7 @@ export function MembraneLipidsMembraneFluidity1() {
           <h1>The Hydrophobic Effect Holds the Bilayer Together</h1>
           <p>
             When lipids are added to water, lipid bilayer sheets{" "}
-            <Tooltip title="define">
+            <Tooltip title="the method by which phospholipids create a bilayer by maximizing the number of hydrogen bonds between the head of the phospholipids and the water molecules outside and inside the cell">
               <span style={{ textDecoration: "underline" }}>
                 <strong>self-assemble</strong>
               </span>
@@ -57,7 +57,7 @@ export function MembraneLipidsMembraneFluidity1() {
           <p>
             The hydrophobic effect can be thought of as "the Jaw of maximization
             of{" "}
-            <Tooltip title="define">
+            <Tooltip title="the bonds formed between two polar molecules and the force that initiates the self assembly of phospholipids">
               <span style={{ textDecoration: "underline" }}>
                 <strong>hydrogen bonding</strong>
               </span>
@@ -69,7 +69,7 @@ export function MembraneLipidsMembraneFluidity1() {
             bonding, more and more hydrogen bonds are formed. Each time a new
             hydrogen bond is formed, an oil molecule is pushed away, or
             excluded. This process explains why{" "}
-            <Tooltip title="define">
+            <Tooltip title="a characteristic of a molecule in which the covalent bonds between atoms aren't equal and one atom is more 'greedy' for the electrons being shared">
               <span style={{ textDecoration: "underline" }}>
                 <strong>polar</strong>
               </span>
@@ -99,19 +99,19 @@ export function MembraneLipidsMembraneFluidity1() {
           <p>
             A major difference between membrane lipids and oil is that while oil
             is non-polar, membrane lipids are{" "}
-            <Tooltip title="define">
+            <Tooltip title="a molecule with a hydrophilic 'head' and a hydrophobic 'tail'">
               <span style={{ textDecoration: "underline" }}>
                 <strong>amphipathic</strong>
               </span>
             </Tooltip>{" "}
             . The{" "}
-            <Tooltip title="define">
+            <Tooltip title="compounds that tend to repel water and are not soluble in water (not polar)">
               <span style={{ textDecoration: "underline" }}>
                 <strong>hydrophobic</strong>
               </span>
             </Tooltip>{" "}
             lipid tails are excluded by water, while the{" "}
-            <Tooltip title="define">
+            <Tooltip title="compounds that tend to be attracted to water and soluble in water (polar)">
               <span style={{ textDecoration: "underline" }}>
                 <strong>hydrophilic</strong>
               </span>
@@ -142,7 +142,7 @@ export function MembraneLipidsMembraneFluidity1() {
           <h1>The Hydrophobic Effect Holds the Bilayer Together</h1>
           <p>
             This figure overlay demonstrates how{" "}
-            <Tooltip title="define">
+            <Tooltip title="the bonds formed between two polar molecules and the force that initiates the self assembly of phospholipids">
               <span style={{ textDecoration: "underline" }}>
                 <strong>hydrogen bonding</strong>
               </span>
@@ -150,7 +150,7 @@ export function MembraneLipidsMembraneFluidity1() {
             is maximized by the lipid bilayer. In a random mixture of lipids and
             water, lipid tails intercalate between water molecules, interfering
             with hydrogen bonding. The formation of the{" "}
-            <Tooltip title="define">
+            <Tooltip title="the membrane that consistes of a ring of phospholipids with the heads pointing out and a ring of phospholipids with the heads pointing in that encompasses a cell">
               <span style={{ textDecoration: "underline" }}>
                 <strong>lipid bilayer</strong>
               </span>
@@ -160,19 +160,19 @@ export function MembraneLipidsMembraneFluidity1() {
           <p>
             Once lipid bilayers have formed, the membrane lipids are made more
             cohesive by weak{" "}
-            <Tooltip title="define">
+            <Tooltip title="bonds between atoms that do not involve the sharing of atoms">
               <span style={{ textDecoration: "underline" }}>
                 <strong>noncovalent</strong>
               </span>
             </Tooltip>{" "}
             forces:{" "}
-            <Tooltip title="define">
+            <Tooltip title="very weak interactions between nonpolar molecules that are caused by the slight attractions of synced up electrons on different molecules">
               <span style={{ textDecoration: "underline" }}>
                 <strong>van der Waals</strong>
               </span>
             </Tooltip>{" "}
             attractive forces are at work among the hydrocarbon tails, and{" "}
-            <Tooltip title="define">
+            <Tooltip title="charges caused by the difference in electron charge of different molecules">
               <span style={{ textDecoration: "underline" }}>
                 <strong>electrostatic</strong>
               </span>
@@ -203,7 +203,7 @@ export function MembraneLipidsMembraneFluidity1() {
             interactions, the lipids and many of the associated proteins in the
             membrane are not rigidly held in place and can move around quite
             easily within the same plane. This movement is referred to as{" "}
-            <Tooltip title="define">
+            <Tooltip title="a measure of the extent to which the phospholipids of the bilayer are loose">
               <span style={{ textDecoration: "underline" }}>
                 <strong>membrane fluidity</strong>
               </span>
@@ -214,15 +214,21 @@ export function MembraneLipidsMembraneFluidity1() {
             rare. Thus, the fluidity of cell membranes is confined to two
             dimensions.
           </p>
-          <p>
-            <strong>Can membrane lipids rotate? </strong> Yes
-          </p>
-          <p>
-            <strong>Can membrane lipids flip-flop? </strong> No
-          </p>
-          <p>
-            <strong>Can membrane lipids move laterally? </strong> Yes
-          </p>
+          <Tooltip title="Yes">
+            <p>
+              <strong>Can membrane lipids rotate? </strong>
+            </p>
+          </Tooltip>
+          <Tooltip title="No">
+            <p>
+              <strong>Can membrane lipids flip-flop? </strong>
+            </p>
+          </Tooltip>
+          <Tooltip title="Yes">
+            <p>
+              <strong>Can membrane lipids move laterally? </strong>
+            </p>
+          </Tooltip>
         </div>
         <div className="imagecontainer">
           <img src={membranefluidity15} />
--- website/src/membraneproteins/integralandperipheral.js
+++ website/src/membraneproteins/integralandperipheral.js
@@ -16,28 +16,33 @@ export function MembraneProteinIntegralAndPeripheral() {
             Membrane proteins are crucial both for membrane function and
             membrane stability. Different membranes differ radically in their
             protein content. For example, the protein content of the{" "}
-            <span style={{ textDecoration: "underline" }}>
-              <strong>plasma membrane</strong>
-            </span>{" "}
+            <Tooltip title="a biological membrane that regulates passage of materials in and out of the cell and provides support and protection of the cell">
+              <span style={{ textDecoration: "underline" }}>
+                <strong>plasma membrane</strong>
+              </span>
+            </Tooltip>{" "}
             is typically 50%, while the protein content of{" "}
-            <span style={{ textDecoration: "underline" }}>
-              <strong>organelle</strong>
-            </span>{" "}
+            <Tooltip title="areas of the cell enclosed by a biological membrane to execute different functions needed for the cell to live">
+              <span style={{ textDecoration: "underline" }}>
+                <strong>organelle</strong>
+              </span>
+            </Tooltip>{" "}
             membranes involved in energy generation (i.e. the internal membranes
             of mitochondria and chloroplasts) is upwards of 75%.
             <p> </p>Proteins that are bound to only one side of the lipid
             bilayer are called{" "}
-            <span style={{ textDecoration: "underline" }}>
-              <strong>peripheral membrane proteins</strong>
-            </span>
+            <Tooltip title="these proteins are only on one side of the membrane and thus are hydrophilic">
+              <span style={{ textDecoration: "underline" }}>
+                <strong>peripheral membrane proteins</strong>
+              </span>
+            </Tooltip>
             . Proteins that interact directly with the hydrophobic portion of
             membranes are termed{" "}
-            <span style={{ textDecoration: "underline" }}>
-              <strong>integral membrane</strong>
-            </span>
-            <span style={{ textDecoration: "underline" }}>
-              <strong>proteins</strong>
-            </span>
+            <Tooltip title="a membrane that passes through the bilayer and thus, the part of the protein inside the bilayer is hydrophobic and the parts of the membrane outside the bilayer are hydrophilic">
+              <span style={{ textDecoration: "underline" }}>
+                <strong>integral membrane proteins</strong>
+              </span>
+            </Tooltip>
             . In some cases, these integral membrane proteins pass completely
             through a membrane. In other cases, these proteins interact with the
             membrane via a portion embedded in only one monolayer of the
@@ -64,15 +69,19 @@ export function MembraneProteinIntegralAndPeripheral() {
           <p>
             Most integral membrane proteins span the lipid bilayer with either a
             single{" "}
-            <span style={{ textDecoration: "underline" }}>
-              <strong>helix</strong>
-            </span>{" "}
+            <Tooltip title="a characteristic of the secondary structure of a protein shaped by hydrogen bonds between the bases of the amino acids">
+              <span style={{ textDecoration: "underline" }}>
+                <strong>helix</strong>
+              </span>
+            </Tooltip>{" "}
             (often called a single-pass transmembrane proteins) or several
             helices (multipass transmembrane proteins). The membrane-spanning
             helices, called{" "}
-            <span style={{ textDecoration: "underline" }}>
-              <strong>transmembrane domains</strong>
-            </span>
+            <Tooltip title="the parts of the protein that are in the bilayer itself and thus interact with the hydrophobic tails of the membrane">
+              <span style={{ textDecoration: "underline" }}>
+                <strong>transmembrane domains</strong>
+              </span>
+            </Tooltip>
             , interact with the hydrophobic tails of membrane lipids and anchor
             the protein in the membrane. The parts of the protein that are
             exposed to water on either side of the membrane are largely
@@ -102,9 +111,11 @@ export function MembraneProteinIntegralAndPeripheral() {
             with extracellular substances may have a large region exposed to the
             outside of the cell but only a small polypeptide portion exposed to
             the{" "}
-            <span style={{ textDecoration: "underline" }}>
-              <strong>cytosol</strong>
-            </span>
+            <Tooltip title="the gel that is located inside the cell but outside the organelles that contain water and other biological molecules">
+              <span style={{ textDecoration: "underline" }}>
+                <strong>cytosol</strong>
+              </span>
+            </Tooltip>
             . The particular orientation of a protein is established during its
             synthesis. Due to the extensive polar regions on either side of the
             membrane, integral membrane proteins do not flip transversely
